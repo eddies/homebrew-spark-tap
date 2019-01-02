ce5f910356f9443783241e946e56a2a0d206f5e8
Edwin Shin <eddies@users.noreply.github.com>
2019-01-02 17:06:54 +0800

    Adds formula for 2.3.2
    (Based on homebrew/core https://github.com/Homebrew/homebrew-core/blob/0f3992a1e6d036dab8b420903d856231e7701ba1/Formula/apache-spark.rb)

---
+++ Formula/apache-spark@2.3.2.rb
@@ -0,0 +1,26 @@
+class ApacheSparkAT232 < Formula
+  desc "Engine for large-scale data processing"
+  homepage "https://spark.apache.org/"
+  url "https://archive.apache.org/dist/spark/spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz"
+  version "2.3.2"
+  sha256 "6246b20d95c7596a29fb26d5b50a3ae3163a35915bec6c515a8e183383bedc43"
+  head "https://github.com/apache/spark.git"
+
+  bottle :unneeded
+
+  depends_on :java => "1.8"
+
+  def install
+    # Rename beeline to distinguish it from hive's beeline
+    mv "bin/beeline", "bin/spark-beeline"
+
+    rm_f Dir["bin/*.cmd"]
+    libexec.install Dir["*"]
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+  end
+
+  test do
+    assert_match "Long = 1000", pipe_output(bin/"spark-shell", "sc.parallelize(1 to 1000).count()")
+end
+end
