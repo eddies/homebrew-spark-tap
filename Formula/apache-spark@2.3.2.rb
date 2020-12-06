df0c8bba08150ce91ad9ef0d0c4677b6e9a336a1
Raffael Dzikowski <raffael.dzikowski@autoscout24.com>
2020-12-06 12:00:39 +0100

    Solve Dependency Resolution Problem for Java 8
    This PR proposes replacing the deprecated `depends_on :java => "1.8"`
    with the appropriate `openjdk` version.

--- Formula/apache-spark@2.3.2.rb
+++ Formula/apache-spark@2.3.2.rb
@@ -8,7 +8,7 @@ class ApacheSparkAT232 < Formula
 
   bottle :unneeded
 
-  depends_on :java => "1.8"
+  depends_on "openjdk@8"
 
   def install
     # Rename beeline to distinguish it from hive's beeline
